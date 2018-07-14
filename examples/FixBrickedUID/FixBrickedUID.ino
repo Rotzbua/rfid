5f7259d50720895987f08ad7f0516bfb0df3d618
Rotzbua <Rotzbua@users.noreply.github.com>
2018-07-15 01:17:51 +0200

    fix examples

--- examples/FixBrickedUID/FixBrickedUID.ino
+++ examples/FixBrickedUID/FixBrickedUID.ino
@@ -30,7 +30,7 @@ constexpr uint8_t RST_PIN = 9;     // Configurable, see typical pin layout above
 constexpr uint8_t SS_PIN = 10;     // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
-MFRC522Hack mfrc522Hack(&mfrc522);  // Create MFRC522Hack instance.
+MFRC522Hack mfrc522Hack(mfrc522);  // Create MFRC522Hack instance.
 
 MFRC522::MIFARE_Key key;
 
